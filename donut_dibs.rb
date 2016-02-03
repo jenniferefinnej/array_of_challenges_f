b0df8708d7b02a9e5bc6fc256453a03075faa64d
Jennifer Nguyen <j3nniferefinn3j@gmail.com>
2016-02-02 23:15:11 -0600

    finished

--- donut_dibs.rb
+++ donut_dibs.rb
@@ -5,10 +5,15 @@ donut_box2 = ["Choc Sprinkles", "Choc Sprinkles", "double choc cake", "double ch
 #There are two boxes of delicious donuts sitting in front of you. They're everyrthing you dreamed of and more. 
 #But there's a problem. We don't know which donut belongs to which one of your classmates!
 
+donut_box1 = Hash["Boston Creme" => "Jennifer", "Choc Long John" => "Michelle", "Vanilla Long John" => "Kelly"] 
+puts "#{donut_box1 ['Boston Creme']}"
 
+# => hash rocket
 
 #Create a hash where donuts are assigned to specific classmates
 
+Jessica = {"Lena" => "Old Fashioned", "Kelly" => "double choc cake", "Vedant" => "glazed"}
+print Jessica
 
 #Return the donut of a specific person
 
@@ -18,5 +23,14 @@ donut_box2 = ["Choc Sprinkles", "Choc Sprinkles", "double choc cake", "double ch
 
 
 #BONUS: Iterate through the hash and print a sentence for each student/donut pair. Example: "James calls dibs on a chocolate cake donut!"
+puts "What's your name?"
+name = gets.chomp
+
+puts "What donut do you want?"
+
+hash.each do |name, donut|
+	puts "#{name}"
+	puts donut
+end
 
 
