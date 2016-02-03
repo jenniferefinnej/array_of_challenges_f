8736f33479bb182a86e9ad6f12a3a5155cff8f2a
Jennifer Nguyen <j3nniferefinn3j@gmail.com>
2016-02-02 23:04:29 -0600

    all done

--- array_of_challenges.rb
+++ array_of_challenges.rb
@@ -1,17 +1,31 @@
 #Line up Alphabetically 
 	#Sort all the names in this array alphabetically
 	star_students = [ "Kelly", "Henry", "Theo", "Jennifer", "Michelle"]
+	
+	#star_students.sort!
+	#sort! makes it alphabetical
+	# ! destructive though
+	sorted_star_students = star_students.sort
 
-puts star_students[2]
+	#print displays the stuff above
+
+puts Array.new(19, "Jennifer")
 	#create a new array that has 19 elements of only your name as a string
 		#(hint: There is a way do this with only typing the string once)
-
+#puts makes everything on a new line vs print, which puts everything on the same line
 
 	#That first array, star_students and this array, brilliant_students, should be one big array!
 		#combine this array with the one above
 		#Then sort the final array in alphabetic order
 
 	brilliant_students = ["Vedant", "Lena", "Zara", "Jessica"]
+
+	brilliant_star_students = star_students + brilliant_students
+
+		#concat is the same as using the plus sign +
+	brilliant_star_students.sort!
+	print brilliant_star_students
+
 
 	
 	#BONUS
@@ -22,3 +36,12 @@ puts star_students[2]
 
 			adjectives = ["adventurous", "ambitious", "brave", "bright", "charming", "courageous", "creative", "determined", "dynamic", "diplomatic", "enthusiastic", "helpful", "honest", "humorous", "inventive", "persistent", "powerful", "reliable", "resourceful", "sincere", "thoughtful", "witty"]
 
+			
+			brilliant_star_students.each_with_index { |name, adjectives| puts "#{name} => #{adjectives}"}
+			print brilliant_star_students.each_with_index 
+
+			
+			#brilliant_star_students.each do |adjectives|
+			#print brilliant_star_students
+			
+			
